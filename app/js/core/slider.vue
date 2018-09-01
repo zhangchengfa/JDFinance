4428dcc118f84679b35e3813460612446afc77b9
cephas <215440666@qq.com>
2018-09-01 18:18:41 +0800

    home page develop

---
+++ app/js/core/slider.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+  <section :class="cname">
+    <swiper :options="options" :not-next-tick="options.notNextTick">
+      <swiper-slide v-for="item in items" :key="item.href">
+        <router-link :to="{ name: item.href }">
+          <img :src="item.src" alt="">
+        </router-link>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination" v-if="options.pagination" />
+    </swiper>
+  </section>
+</template>
+
+<script>
+import {swiper,swiperSlide} from 'vue-awesome-swiper'
+export default {
+  components: {
+    swiper,
+    swiperSlide,
+  },
+  props: {
+    opotions: {
+      type: Object,
+      default() {
+        return {
+          autoplay: true,
+          loop: true,
+          pagination: {
+            el: ".swiper-pagination",
+          },
+          notNextTick: false,
+        }
+      }
+    },
+    items: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  @import "~swiper/dist/css/swiper.css";
+</style>
